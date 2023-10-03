2012071eda1ce20730f6ce1434bf86d5129f338e
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-10-03 11:05:23 -0500

    added a few options to JS

--- js/app.js
+++ js/app.js
@@ -3,18 +3,22 @@
 let health = 3;
 let playerName;
 
+function changeText(name) {
+    text.innerHTML = name.replace("Player Name", playerName);
+};
+
 //are we doing images? map?
 
-// // let images = document.getElementById("images");
-// let text = document.getElementById('text');
-// let buttonBox = document.getElementById('buttonBox');
-// let input = document.getElementById('input');
+// let images = document.getElementById("images");
+let text = document.getElementById('text');
+let buttonBox = document.getElementById('buttonBox');
+let input = document.getElementById('input');
 
-// function nextQ(num) {
-//   //   changeImage(num.image);
-//   changeText(num.text);
-//   changeButtons(num.buttons);
-// }
+function nextQ(num) {
+    // changeImage(num.image);
+  changeText(num.text);
+  changeButtons(num.buttons);
+}
 
 // addEventListener("click", nextQ()) = 
 
@@ -37,19 +41,30 @@ let playerName;
 //     nextQ();
 // };
 
-const scenario {
-    one: {
-        text:'Your car has broken down. Do you run up the stairs or walk',
-        buttons: [[Run], [Walk]]
-    },
-    two: {
-        text: 'There is a stag head on the wall. Do you look at it?',
-        buttons: [[Yes], [No]],
-    },
-    three: {
-        text: 'There is a record player. Do you play spooky music or fun music?',
-        buttons: [[Spooky], [fun]]
-    }
+//Note: use "Player Name" for the player name so that the function replaces it correctly
+
+function questionOne('click') {
+    let text = 'Your car has broken down. Do you run up the stairs or walk';
+    let buttons = ["Run", "Walk"]
+    addEventListener('click', )
+};
+
+function questionTwo('click') {
+    text = 'There is a stag head on the wall. Do you look at it?';
+    buttons = ["Yes", "No"];
+}
+
+function questionThree('click') {
+    text = 'There is a record player. Do you play spooky music or fun music?';
+    buttons = ["Spooky", "Fun"]
+}
+
+if (scenario.one.buttons[0]) {
+    hearts--;
+};
+
+while (hearts >= 1) {
+
 }
 
 // nextQ(scenario.one);
