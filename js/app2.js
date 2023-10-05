a161ff00da4fdce9d85808cb937997c58c637ffe
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-10-05 15:40:06 -0500

    added alerts and changed control flow to handle minus ten for basement

--- js/app2.js
+++ js/app2.js
@@ -44,16 +44,18 @@ function checkGameOver() {
 
 let scenario = {
   two: {
-    text: 'You\'re driving down a dark, remote road when your front passenger tire blows! You struggle to regain control as you skid off the road.You gather yourself and see a foreboding mansion ahead of you.You need to fix your tire, but you know you do not have the tools.What do you do',
+    text: 'You\'re on your way to a Halloween party, driving down a dark, remote road when your front passenger tire blows! You struggle to regain control as you skid off the road.You gather yourself and see a foreboding mansion ahead of you.You need to fix your tire, but you know you do not have the tools.What do you do',
     buttons: [
+      ['Enter house slowly', function () {
+        alert('Good idea, it\'s better to take your time, especially since you\'re wearing an enormous pumpkin costume.');
+        console.log('Your health is ' + health + 'out of 3');
+        advanceTo('three');
+      }],
       ['Run to entrance', function () {
+        alert('It\'s not a good idea to run in your enormous pumpkin costume. Health -1');
         health--;
         console.log('Your health is ' + health + ' out of 3');
-        advanceTo('three');
         updateHealth();
-      }],
-      ['Enter house slowly', function () {
-        console.log('Your health is ' + health + 'out of 3');
         advanceTo('three');
       }]
     ]
@@ -62,13 +64,13 @@ let scenario = {
     text: 'You enter a dusty sitting room, filled with old volumes and leather seats. You notice an old mounted stag head on the wall. Did the eyes just move?',
     buttons: [
       ['Inspect', function () {
-        health--;
         alert('It falls off the wall and impales you. Health -1');
+        health--;
         console.log('Your health is ' + health + ' out of 3');
-        advanceTo('four');
         updateHealth();
+        advanceTo('four');
       }],
-      ['Ignore and go to next room', function () {
+      ['Ignore it and go to next room', function () {
         alert('Good idea. Best not to stare into its eyes too long.');
         console.log('Your health is ' + health + 'out of 3');
         advanceTo('four');
@@ -85,9 +87,9 @@ let scenario = {
         alert('Why would you play scary music. Health -1');
         health--;
         console.log('Your health is ' + health + ' out of 3');
+        updateHealth();
         advanceTo('five');
         checkGameOver();
-        updateHealth();
       }],
       ['Funny', function () {
         alert('Good choice. Nothing bad happens when the music is silly.');
@@ -106,13 +108,14 @@ let scenario = {
         alert('That wasn\'t a chest, it was a mimic! That\'s what you get for stealing. Health -1');
         health--;
         console.log('Your health is ' + health + ' out of 3');
+        updateHealth();
         advanceTo('six');
         checkGameOver();
       }],
       ['No', function () {
+        alert('Yes, it\'s not a good idea to mess with strangers\' stuff.');
         console.log('Your health is ' + health + ' out of 3');
         advanceTo('six');
-        updateHealth();
       }]
     ]
   },
@@ -120,17 +123,18 @@ let scenario = {
     text: 'Do you look in the shed or the basement next?',
     buttons: [
       ['Shed', function () {
+        alert('In the shed you find a car jack, which you use to fix your car.');
         console.log('Your health is ' + health + ' out of 3');
         window.location.href = 'win.html';
         advanceTo('seven');
       }],
       ['Basement', function () {
-        alert('Don\'t go in the basement! You fell down the stairs. Health -1');
-        health--;
-        console.log('Your health is ' + health + ' out of 3');
-        advanceTo('seven');
-        checkGameOver();
+        alert('Don\'t go in the basement! You fell down the stairs. Health -10');
+        health -= 10;
         updateHealth();
+        console.log('Your health is ' + health + ' out of 3');
+        // advanceTo('seven');
+        checkGameOver();
       }]
     ]
   }
@@ -142,20 +146,21 @@ let skullImg3 = document.getElementById('skull3');
 
 
 function updateHealth() {
-  if (health === 2) {
+  if (health <= 2) {
     skullImg3.classList.add('fade-out');
     skullImg3.addEventListener('animationend', function () {
       skullImg3.classList.remove('fade-out');
       document.getElementById('skull3').src = 'images/100Purple.png';
     });
-  } else if (health === 1) {
+  }
+  if (health <= 1) {
     skullImg2.classList.add('fade-out');
     skullImg2.addEventListener('animationend', function () {
       skullImg2.classList.remove('fade-out');
       document.getElementById('skull2').src = 'images/100Purple.png';
     });
-
-  } else if (health === 0) {
+  }
+  if (health <= 0) {
     skullImg1.classList.add('fade-out');
     skullImg1.addEventListener('animationend', function () {
       skullImg1.classList.remove('fade-out');
