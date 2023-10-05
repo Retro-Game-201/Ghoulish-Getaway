415aedfc0126332c8ea26a3fe6568d376d229296
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-10-05 13:14:08 -0500

    added returned name almost

---
+++ js/win.js
@@ -0,0 +1,8 @@
+let userName = localStorage.getItem('userName');
+console.log('user name is: ', userName);
+
+let winMsg = document.getElementById.innerHTML = ('winMsg');
+console.log('winMsg is: ', winMsg);
+
+winMsg = `Congratulations, ${userName}, you won!`;
+console.log('reassigned winMgn is: ', winMsg);
